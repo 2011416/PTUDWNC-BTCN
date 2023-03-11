5f969278f95dc9e3627dac382ce313d21b84f3c8
Nhan <2011416@dlu.edu.vn>
2023-03-11 20:09:04 +0700

    l3

---
+++ TatBlog.WebApp/Areas/Admin/Controllers/PostsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace TatBlog.WebApp.Areas.Admin.Controllers
+{
+    public class PostsController :Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+    }
+}
