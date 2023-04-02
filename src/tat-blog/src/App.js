294afa7eba239a648cdee49da54aaa94cf41e41a
2011416_nhan <2011416@dlu.edu.vn>
2023-04-02 14:47:01 +0700

    add sidebar,footer, image

--- src/tat-blog/src/App.js
+++ src/tat-blog/src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import Sidebar from './Components/Sidebar';
+import Footer from './Components/Footer';
+import Index from './Pages/Index';
+import Layout from './Pages/Layout';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import RSS from './Pages/Rss';
 
 import {
   BrowserRouter as Router,
@@ -13,6 +20,25 @@ function App()
     <div>
       <Router>
         <Navbar />
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col-9'>
+              <Routes>
+                <Route path='/' element={<Layout/>}>
+                <Route path='/' element={<Index/>}/>
+                <Route path='blog' element={<Index/>}/>
+                <Route path='blog/Contact' element={<Contact/>}/>
+                <Route path='blog/About' element={<About/>}/>
+                <Route path='blog/RSS' element={<RSS/>}/>
+                </Route>
+              </Routes>            
+            </div>
+            <div className='col-3 border-start'>
+              <Sidebar />
+            </div>
+          </div>
+        </div>
+        <Footer />
       </Router>
     </div>
   );
